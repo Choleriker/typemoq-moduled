d1c9056a733b602cc19314a68c35403e3140bc3e
Michael Baarz <mbaarz@gmail.com>
2016-10-31 14:54:59 +0100

    Exported additional types needed.

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
 export * from './src';
+export * from './src/Mock';
+export * from './src/It';
+export * from './src/Times';
+export * from './src/GlobalMock';
+export * from './src/GlobalScope';
+export * from  './src/Error/MockException';
